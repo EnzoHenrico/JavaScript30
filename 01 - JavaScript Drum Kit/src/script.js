c72c6d8dcf3f9faba9cec0cebb733605c4690bd2
EnzoHenrico <enzo.mascia1@gmail.com>
2022-08-01 18:18:48 -0300

    Day 1 done

--- 01 - JavaScript Drum Kit/src/script.js
+++ 01 - JavaScript Drum Kit/src/script.js
@@ -1,11 +1,33 @@
-window.addEventListener("keydown", (e) => {
-  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-  audio.play();
-});
+window.addEventListener("keydown", playAudio);
 
+function playAudio(e) {
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+
+  if (!audio) return;
+
+  animation(key);
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function animation(key) {
+  const keys = document.querySelectorAll(".key");
+  key.classList.add("keyPlaying");
+  keys.forEach((key) => key.addEventListener("transitionend", removeAnimation));
+}
+
+function removeAnimation(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("keyPlaying");
+}
+
+// Handle mouse clicks on keys
 function handleClick(e) {
-  console.log(e.target.parentElement.dataset);
   const { key } = e.target.parentElement.dataset;
   const audio = document.querySelector(`audio[data-key="${key}"]`);
+
+  animation(e.target.parentElement);
+  audio.currentTime = 0;
   audio.play();
 }
